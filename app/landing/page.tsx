ea2ec6cdf5f4159724ab554a3d0bde39e0efd9d1
Ginazon <72575771+Ginazon@users.noreply.github.com>
2025-12-12 00:10:49 +0300

    landing page landing

--- app/landing/page.tsx
+++ app/landing/page.tsx
@@ -123,7 +123,7 @@ export default function LandingPage() {
                         <div className="lg:w-1/2 mt-10 lg:mt-0 relative">
   
                            <img 
-                           src="https://knjwmbwrgcojxcbmrbtm.supabase.co/storage/v1/object/public/brand/1.png"
+                           src="https://knjwmbwrgcojxcbmrbtm.supabase.co/storage/v1/object/public/brand/landing.png"
                            alt="Brand Screenshot"
                          className="w-full h-96 object-cover rounded-xl shadow-2xl"
                               />
